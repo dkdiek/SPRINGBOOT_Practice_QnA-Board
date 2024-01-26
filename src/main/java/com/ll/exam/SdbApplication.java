198d4bdbc2e9814c8b17251c1a0ebe58e04d93f8
developerlyk <developerlyk@gmail.com>
2024-01-26 13:44:17 +0900

    /maincontroller::index method 추가

--- src/main/java/com/ll/exam/SdbApplication.java
+++ src/main/java/com/ll/exam/SdbApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SdbApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(SdbApplication.class, args);
     }
