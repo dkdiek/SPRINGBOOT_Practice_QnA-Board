07b680e447a017744f1efd588953ed20eccd15a7
k <developerlyk@gmail.com>
2024-01-27 20:09:26 +0900

    Junit test assertthat 추가

--- src/test/java/com/ll/exam/SdbApplicationTests.java
+++ src/test/java/com/ll/exam/SdbApplicationTests.java
@@ -8,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 @SpringBootTest
 class SdbApplicationTests {
 
@@ -27,6 +30,11 @@ class SdbApplicationTests {
         q2.setContent("id는 자동으로 생성되나요?");
         q2.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q2);  // 두번째 질문 저장
+
+        assertThat(q1.getId()).isGreaterThan(0); //id가 최소 0보다 크다
+        assertThat(q2.getId()).isGreaterThan(q1.getId()); // 1번째 아이디보다 크다
+
+
     }
 
 }
