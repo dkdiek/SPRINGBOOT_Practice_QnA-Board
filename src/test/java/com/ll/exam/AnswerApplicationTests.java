7f00a47cda2b74b7659349943677a6c35c6b6ddd
k <developerlyk@gmail.com>
2024-01-28 15:46:58 +0900

    answerRepository 저장 케이스 추가

---
+++ src/test/java/com/ll/exam/AnswerApplicationTests.java
@@ -0,0 +1,31 @@
+package com.ll.exam;
+
+import com.ll.exam.answer.Answer;
+import com.ll.exam.answer.AnswerRepository;
+import com.ll.exam.question.Question;
+import com.ll.exam.question.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class AnswerApplicationTests {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+    
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    void 저장() {
+        Question q = questionRepository.findById(2).get();
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a);
+    }
+}
