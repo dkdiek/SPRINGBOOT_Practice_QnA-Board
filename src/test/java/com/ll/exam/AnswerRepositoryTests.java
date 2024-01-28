144142c5a4ba24572be6e462e358138d67f4863a
k <developerlyk@gmail.com>
2024-01-28 18:13:59 +0900

    answer 조회

--- src/test/java/com/ll/exam/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/AnswerRepositoryTests.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 public class AnswerRepositoryTests {
 
@@ -34,6 +36,21 @@ public class AnswerRepositoryTests {
 
     private void createSampleData() {
         QuestionRepositoryTests.createSampleData(questionRepository);
+
+        Question q = questionRepository.findById(1).get();
+
+        Answer a1 = new Answer();
+        a1.setContent("SBB는 질문답변 게시판입니다");
+        a1.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a1.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a1); 
+        
+        Answer a2 = new Answer();
+        a2.setContent("SBB에서는 주로 스프링관련 내용을 다룹니다");
+        a2.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+        a2.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a2);
+
     }
 
 
@@ -47,6 +64,11 @@ public class AnswerRepositoryTests {
         answerRepository.save(a);
     }
 
+    @Test
+    void 조회() {
+        Answer a = this.answerRepository.findById(1).get();
+        assertThat(a.getContent()).isEqualTo("SBB는 질문답변 게시판입니다");
+    }
 
 
 
