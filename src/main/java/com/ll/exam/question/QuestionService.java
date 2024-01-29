e3edf7177185473461397a57012183ce831c3ed3
developerlyk <developerlyk@gmail.com>
2024-01-29 16:50:34 +0900

    질문 등록 처리

--- src/main/java/com/ll/exam/question/QuestionService.java
+++ src/main/java/com/ll/exam/question/QuestionService.java
@@ -4,6 +4,7 @@ import com.ll.exam.DataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -23,5 +24,13 @@ public class QuestionService {
     public Question getQuestion(int id) {
         return questionRepository.findById(id)
                 .orElseThrow(() -> new DataNotFoundException("no %d question not found".formatted(id)));
+    }
+
+    public void create(String subject, String content) {
+            Question q = new Question();
+            q.setSubject(subject);
+            q.setContent(content);
+            q.setCreateDate(LocalDateTime.now());
+            this.questionRepository.save(q);
     }
 }
