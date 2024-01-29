f22ce26243435409ac6227872a2844ef58f37fbf
developerlyk <developerlyk@gmail.com>
2024-01-29 14:13:55 +0900

    상세페이지 작업

---
+++ src/main/java/com/ll/exam/DataNotFoundException.java
@@ -0,0 +1,6 @@
+package com.ll.exam;
+
+public class DataNotFoundException extends RuntimeException {
+    public DataNotFoundException(String questionNotFound) {
+    }
+}
--- src/main/java/com/ll/exam/question/QuestionService.java
+++ src/main/java/com/ll/exam/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.ll.exam.question;
 
+import com.ll.exam.DataNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -19,4 +21,13 @@ public class QuestionService {
         return questionRepository.findAll();
     }
 
+    public Question getQuestion(int id) {
+        Optional<Question> oq = questionRepository.findById(id);
+
+
+            if (oq.isPresent()) {
+                return oq.get();
+            }
+            throw new DataNotFoundException("question not found");
+    }
 }
