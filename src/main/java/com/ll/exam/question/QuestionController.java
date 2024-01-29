a3186bdc0188dbbf9dfff688d8677a1a61c97e30
developerlyk <developerlyk@gmail.com>
2024-01-29 11:44:18 +0900

    게시물리스트 테이블 생성,타임리프 캐시 disable

--- src/main/java/com/ll/exam/question/QuestionController.java
+++ src/main/java/com/ll/exam/question/QuestionController.java
@@ -1,14 +1,22 @@
 package com.ll.exam.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor// 생성자 주입 final을 자동으로 autowired시킴
 public class QuestionController {
+    //@AutoWired 필드 주입
+    private final QuestionRepository questionRepository;
 
     @GetMapping("/question/list")
-    public String list()
-    {
+    public String list(Model model) {
+        List<Question> questionList = questionRepository.findAll();
+        model.addAttribute("questionList",questionList);
         return "question_list";
     }
 
