358d9d551455035a4ce723c801ac4a9d55f0443f
developerlyk <developerlyk@gmail.com>
2024-01-29 14:32:49 +0900

    answerController 도입

---
+++ src/main/java/com/ll/exam/answer/AnswerController.java
@@ -0,0 +1,26 @@
+package com.ll.exam.answer;
+
+import com.ll.exam.question.Question;
+import com.ll.exam.question.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/answer")
+public class AnswerController {
+
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam(value="content") String content) {
+        Question question = this.questionService.getQuestion(id);
+        // 답변 등록 시작
+        return String.format("redirect:/question/detail/%s", id);
+    }
+}
