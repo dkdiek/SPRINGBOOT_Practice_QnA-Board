b58e5134d541da0e90b819511e0e40ee9db9e8c0
developerlyk <developerlyk@gmail.com>
2024-01-29 11:20:35 +0900

    question _list 템플릿 추가

--- src/main/java/com/ll/exam/question/QuestionController.java
+++ src/main/java/com/ll/exam/question/QuestionController.java
@@ -2,15 +2,14 @@ package com.ll.exam.question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuestionController {
 
     @GetMapping("/question/list")
-    @ResponseBody
-    public String list() {
-        return "question list";
+    public String list()
+    {
+        return "question_list";
     }
-    
+
 }
