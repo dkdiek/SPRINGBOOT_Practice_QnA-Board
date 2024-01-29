0264cf4557598996848b1762acbab521d1373ef1
developerlyk <developerlyk@gmail.com>
2024-01-29 11:10:07 +0900

    answer를 만드는 방법을 간소화 객체 지향

--- src/test/java/com/ll/exam/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/AnswerRepositoryTests.java
@@ -49,13 +49,11 @@ public class AnswerRepositoryTests {
         a1.setContent("SBB는 질문답변 게시판입니다");
         a1.setCreateDate(LocalDateTime.now());
         q.addAnswer(a1);
-        answerRepository.save(a1);
 
         Answer a2 = new Answer();
         a2.setContent("SBB에서는 주로 스프링관련 내용을 다룹니다");
         a2.setCreateDate(LocalDateTime.now());
         q.addAnswer(a2);
-        answerRepository.save(a2);
 
         questionRepository.save(q);
     }
@@ -66,20 +64,30 @@ public class AnswerRepositoryTests {
     @Rollback(value = false)
     void 저장() {
         Question q = questionRepository.findById(2).get();
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
-        a.setCreateDate(LocalDateTime.now());
-        q.addAnswer(a);
-        answerRepository.save(a);
+
+        Answer a1 = new Answer();
+        a1.setContent("네 자동으로 생성됩니다.");
+        a1.setCreateDate(LocalDateTime.now());
+        q.addAnswer(a1);
+
+        Answer a2 = new Answer();
+        a2.setContent("네네 맞습니다!!");
+        a2.setCreateDate(LocalDateTime.now());
+        q.addAnswer(a2);
+
+        questionRepository.save(q);
     }
 
     @Test
+    @Transactional
+    @Rollback(value = false)
     void 조회() {
         Answer a = this.answerRepository.findById(1).get();
         assertThat(a.getContent()).isEqualTo("SBB는 질문답변 게시판입니다");
     }
     @Test
+    @Transactional
+    @Rollback(value = false)
     void 관련된_question_조회() {
         Answer a = this.answerRepository.findById(1).get();
         Question q = a.getQuestion();
