4e5364f4206940cd6fccf103c261e1b8c58c4110
developerlyk <developerlyk@gmail.com>
2024-01-30 13:29:02 +0900

    스프링 시큐리티 모든 접속 허용

---
+++ src/main/java/com/ll/exam/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.ll.exam;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        // 모든 경로
+                        .requestMatchers(new AntPathRequestMatcher("/**"))
+                        // 허용 한다
+                        .permitAll())
+        ;
+        return http.build();
+    }
+
+}
