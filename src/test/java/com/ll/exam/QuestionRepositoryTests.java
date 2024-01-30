9387cbd05e78c44180221ea1212a74019bb07690
developerlyk <developerlyk@gmail.com>
2024-01-30 11:14:15 +0900

    페이징 함수 테스트

--- src/test/java/com/ll/exam/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/QuestionRepositoryTests.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -142,6 +145,17 @@ class QuestionRepositoryTests {
         });
     }
 
+    @Test
+    void Pageable(){
+
+        // 한 페이지에 몇 개의 아이템이 나와야 하는지 알려주고 현재 몇 페이지인지 알려준다
+        Pageable pageable = PageRequest.of(0, lastSampleDataId);
+
+        Page<Question> page = questionRepository.findAll(pageable);
+
+        assertThat(page.getTotalPages()).isEqualTo(1);
+
+    }
 
 
 }
