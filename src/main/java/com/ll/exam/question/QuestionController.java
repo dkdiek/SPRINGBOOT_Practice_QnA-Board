8301d2b2f6d1ba7c0d41fb089f18daa258ba3c34
developerlyk <developerlyk@gmail.com>
2024-01-31 14:30:52 +0900

    질문 삭제 처리

--- src/main/java/com/ll/exam/question/QuestionController.java
+++ src/main/java/com/ll/exam/question/QuestionController.java
@@ -101,4 +101,21 @@ public class QuestionController {
         return String.format("redirect:/question/detail/%s", id);
     }
 
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+
+        if(question == null){
+            throw new DataNotFoundException("%d번 질문은 존재하지 않습니다".formatted(id));
+        }
+
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+
+        questionService.delete(question);
+        return "redirect:/";
+    }
+
 }
